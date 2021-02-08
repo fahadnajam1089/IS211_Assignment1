389c73145b4eff1ef862f7f3160d8a2b98bf93d2
fahadnajam1089 <78575323+fahadnajam1089@users.noreply.github.com>
2021-02-08 11:17:26 -0500

    Updated Files for assignment 1

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,22 +1,25 @@
-#default value of divide is 2
-def listDivide(l,divide=2):
-    c=0
-    for e in l:
-        #check all the elements are divisible, if it is divisible then increase c by 1
-        if e%divide == 0:
-            c=c+1
-    return c
+# divide parameter needs to be 2
+
+def listDivide(numbers, divide=2):
+    total = 0
+    for e in numbers:
+
+        # checking elements are divisible and if yes, then increase by 1
+        if e % divide == 0:
+            total = total + 1
+    return total
+
 
 class ListDivideException:
     @property
     def testListDivide(self):
-        if listDivide([1,2,3,4,5])!=2:
+        if listDivide([1, 2, 3, 4, 5]) != 2:
             return "Test Case Failed"
-        elif listDivide([2,4,6,8,10])!=5:
+        elif listDivide([2, 4, 6, 8, 10]) != 5:
             return "Test Case Failed"
-        elif listDivide([30,54,63,98,100],divide=10)!=2:
+        elif listDivide([30, 54, 63, 98, 100], divide=10) != 2:
             return "Test Case Failed"
-        elif listDivide([])!=0:
+        elif listDivide([]) != 0:
             return "Test Case Failed"
-        elif listDivide([1,2,3,4,5],divide=1)!=5:
+        elif listDivide([1, 2, 3, 4, 5], divide=1) != 5:
             return "Test Case Failed"
